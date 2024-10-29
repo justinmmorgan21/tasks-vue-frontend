ac282456b88f52272e93630e5a357249a310d236
justinmmorgan21 <justinmorgan21@gmail.com>
2024-10-29 18:50:13 -0500

    init, add index action

---
+++ src/components/Content.vue
@@ -0,0 +1,38 @@
+<script>
+  import axios from "axios";
+  import TasksIndex from "./TasksIndex.vue";
+
+  export default {
+    components: {
+      TasksIndex,
+    },
+    data: function() {
+      return {
+        tasks: [],
+        // tasks: [
+        //   { id: 1, name: "First", estimated_time: 120, deadline: "2024-11-01" },
+        //   { id: 2, name: "Second", estimated_time: 90, deadline: "2024-11-02" }
+        // ],
+      };
+    },
+    created: function () {
+      this.handleIndexTasks();
+    },
+    methods: {
+      handleIndexTasks: function () {
+        axios.get("http://localhost:5000/tasks.json").then(response => {
+          console.log("tasks index", response);
+          this.tasks = response.data;
+        });
+      },
+    },
+  };
+</script>
+
+<template>
+  <main>
+    <TasksIndex v-bind:tasks="tasks"/>
+  </main>
+</template>
+
+<style></style>
---
+++ src/components/TasksIndex.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  props: {
+    tasks: Array,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>All tasks</h1>
+    <div v-for="task in tasks" v-bind:key="task.id">
+      <h2>{{  task.name }}</h2>
+      <p>estimated time: {{ task.estimated_time }}</p>
+      <p>deadline: {{ task.deadline }}</p>
+      <hr>
+    </div>
+  </div>
+  <br>
+</template>
+
+<style></style>
